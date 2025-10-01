9b45bd5445c44f1dc532947cf258d471b9917f6e
alimvleha <alimovleha@gmail.com>
2025-10-02 02:11:32 +0400

    Параметризация запуска тестов

--- adressbook/src/test/java/tests/TestsBase.java
+++ adressbook/src/test/java/tests/TestsBase.java
@@ -10,7 +10,7 @@ public class TestsBase {
     public void setUp() {
         if (app == null) {
             app = new ApplicationManager();
+            app.init(System.getProperty("browser", "firefox"));
         }
-        app.init();
     }
 }
