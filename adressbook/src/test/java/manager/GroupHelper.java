052317f572996844b24294ed7f24d9db76789653
alimvleha <alimovleha@gmail.com>
2025-10-07 00:58:11 +0400

    Цикл: перебор элементов коллекции testRemoveAllGroupsAtOnce

--- adressbook/src/test/java/manager/GroupHelper.java
+++ adressbook/src/test/java/manager/GroupHelper.java
@@ -26,7 +26,7 @@ public class GroupHelper extends HelperBase {
     public void removeGroup() {
         openGroupsPage();
         selectGroup();
-        removeSelectedGroup();
+        removeSelectedGroups();
         returnToGroupsPage();
     }
 
@@ -68,7 +68,7 @@ public class GroupHelper extends HelperBase {
         click(By.xpath("//input[@name=\'new\']"));
     }
 
-    private void removeSelectedGroup() {
+    private void removeSelectedGroups() {
         click(By.xpath("(//input[@name=\'delete\'])[2]"));
     }
 
@@ -85,5 +85,18 @@ public class GroupHelper extends HelperBase {
     public int getCount() {
         openGroupsPage();
         return manager.driver.findElements(By.name("selected[]")).size();
+    }
+
+    public void removeAllGroups() {
+        openGroupsPage();
+        selectAllGroups();
+        removeSelectedGroups();
+    }
+
+    private void selectAllGroups() {
+        var checkboxes = manager.driver.findElements(By.name("selected[]"));
+        for (var checkbox : checkboxes) {
+            checkbox.click();
+        }
     }
 }
--- adressbook/src/test/java/tests/TestsRemovalGroup.java
+++ adressbook/src/test/java/tests/TestsRemovalGroup.java
@@ -16,4 +16,13 @@ public class TestsRemovalGroup extends TestsBase {
         int newGroupCount = app.groups().getCount();
         Assertions.assertEquals(groupCount - 1, newGroupCount );
     }
+
+    @Test
+    void testRemoveAllGroupsAtOnce(){
+        if (app.groups().getCount() == 0) {
+            app.groups().createGroup(new GroupData("name", "header", "footer"));
+        }
+        app.groups().removeAllGroups();
+        Assertions.assertEquals(0, app.groups().getCount());
+    }
 }
